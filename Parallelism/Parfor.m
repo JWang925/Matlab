0ef17df34b23c422f6a7965448b85b5b01801da9
jiajun <wangjiajun925@gmail.com>
2017-03-05 22:59:10 -0500

    Second update
    completed an simple array manipulation case; speed improve when having 4
    cores = 200%; GPU = 800%.

--- Parallelism/Parfor.m
+++ Parallelism/Parfor.m
@@ -1,13 +1,25 @@
 %No access to common variables outside of parfor loop
-size=100*10^6;
+
+%%Tested in R2015b, the speed improvement when having 4 works is 2.89sec
+%%compared to 5.18 sec.
+size=50*10^6;
+A=zeros(size,1);
 tic
 for i = 1:size
   A(i) = sin(i*2*pi/size);
 end
 toc
 
+A=zeros(size,1);
 tic
 parfor i = 1:size
   A(i) = sin(i*2*pi/size);
 end
+toc
+
+%GPU Memory usage=1.3G , time cost = 0.77sec
+tic
+input=gpuArray(1:size);
+A_gpu=sin(input*2*pi/size); 
+A=gather(A_gpu);
 toc
